2f31d263535f8e910ab2f6fbf99acbb8a13581ee
sam-prettyman <35538856+sam-prettyman@users.noreply.github.com>
2019-07-20 20:49:14 -0600

    enter the commit message for your changes. Lines starting

---
+++ Models/MovieGenreViewModel .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcMovie.Models;
+using System.Collections.Generic;
+
+namespace MVCMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using MVCMovie.Models;
+using MVCMovie;
+using MvcMovie.Models;
+
+namespace MVCMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Emma Smith: My Story",
+                        Rating = "G",
+                        ReleaseDate = DateTime.Parse("2008-4-11"),
+                        Genre = "Historical Drama",
+                        Price = 10.99M
+
+                    },
+
+                    new Movie
+                    {
+                        Title = "The R.M. ",
+                        Rating = "PG",
+                        ReleaseDate = DateTime.Parse("2003-1-31"),
+                        Genre = "Religous Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Singles 2nd Ward",
+                        Rating = "PG",
+                        ReleaseDate = DateTime.Parse("2007-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Mobsters and Mormons",
+                        Rating = "PG",
+                        ReleaseDate = DateTime.Parse("2005-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
